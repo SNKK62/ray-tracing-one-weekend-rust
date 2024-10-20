58331681d32c9fad6dc9456c3b8252d5aee7fe33
SNKK62 <koki.seno62@gmail.com>
2024-10-20 20:57:15 +0900

    generate red sphere

--- src/ray.rs
+++ src/ray.rs
@@ -25,7 +25,19 @@ impl Ray {
         self.origin + self.direction * t
     }
 
+    pub fn hit_sphere(center: &vec3::Point3, radius: f64, r: &Ray) -> bool {
+        let oc = r.origin - *center;
+        let a = r.direction.dot(&r.direction);
+        let b = 2.0 * oc.dot(&r.direction);
+        let c = oc.dot(&oc) - radius * radius;
+        let discriminant = b * b - 4.0 * a * c;
+        discriminant > 0.0
+    }
+
     pub fn color(&self) -> vec3::Color {
+        if Self::hit_sphere(&vec3::Point3::new(0.0, 0.0, -1.0), 0.5, self) {
+            return vec3::Color::new(1.0, 0.0, 0.0); // red
+        }
         let unit_direction = self.direction.unit();
         let t = 0.5 * (unit_direction.y() + 1.0);
         (1.0 - t) * vec3::Color::new(1.0, 1.0, 1.0) + t * vec3::Color::new(0.5, 0.7, 1.0)
