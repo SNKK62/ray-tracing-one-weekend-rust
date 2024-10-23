3601075342a9ba18446a6595df0fb80ba448e80b
SNKK62 <koki.seno62@gmail.com>
2024-10-24 02:28:08 +0900

    remove unused lines

--- src/main.rs
+++ src/main.rs
@@ -57,16 +57,6 @@ fn main() {
 
     print!("P3\n{} {}\n255\n", width, height);
 
-    let viewport_height = 2.0;
-    let viewport_width = aspect_ratio * viewport_height;
-    let focal_length = 1.0;
-
-    let origin = Point3::zero();
-    let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
-    let vertical = Vec3::new(0.0, viewport_height, 0.0);
-    let lower_left_corner =
-        origin - horizontal / 2.0 - vertical / 2.0 - Vec3::new(0.0, 0.0, focal_length);
-
     let mut world = hittable::HittableList::new();
     world.add(Box::new(hittable::Sphere::new(
         &Point3::new(0.0, 0.0, -1.0),
